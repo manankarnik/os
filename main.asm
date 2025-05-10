e244a7c1ce456e7c18646172e63db5e741936b97
Manan Karnik <manankarnik@gmail.com>
2025-05-10 16:23:36 +0530

    Hello world!

--- main.asm
+++ main.asm
@@ -1,4 +1,19 @@
-jmp $
+org 0x7c00
+
+mov ah, 0x0e
+mov si, msg
+print:
+    mov al, [si]
+    cmp al, 0
+    je loop
+    int 0x10
+    lodsb
+    jmp print
+
+loop:
+    jmp $
+
+msg: db "Hello world!", 0x0D, 0x0A, 0
 
 times 510-($-$$) db 0
 dw 0xaa55
