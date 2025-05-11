d0d39f9ad8b2161f731bec2c2bfba6c0a26968da
Manan Karnik <manankarnik@gmail.com>
2025-05-12 00:29:20 +0530

    Print hex

--- main.asm
+++ main.asm
@@ -1,19 +1,48 @@
 org 0x7c00
 
-mov ah, 0x0e
-mov si, msg
+mov dx, 0x1fc6
+call print_hex
+jmp $
+
 print:
-    mov al, [si]
-    cmp al, 0
-    je loop
-    int 0x10
+    pusha
+    mov ah, 0x0e
+.loop:
     lodsb
-    jmp print
+    cmp al, 0
+    je .break
+    int 0x10
+    jmp .loop
+.break:
+    popa
+    ret
 
-loop:
-    jmp $
+print_hex:
+    pusha
+    mov si, hex_pattern
+    mov cl, 12
+    mov di, 2
+.loop:
+    mov bx, dx
+    shr bx, cl
+    and bx, 0x000f
+    mov bx, [bx + hex_table]
+    mov [hex_pattern + di], bl
+    inc di
+    sub cl, 4
+    cmp cl, 0
+    je .break
+    jmp .loop
+.break:
+    mov si, hex_pattern
+    call print
+    popa
+    ret
 
-msg: db "Hello world!", 0x0D, 0x0A, 0
+hex_pattern:
+    db "0x0000", 0x0D, 0x0A, 0
+hex_table:
+    db "0123456789abcdef"
 
 times 510-($-$$) db 0
 dw 0xaa55
